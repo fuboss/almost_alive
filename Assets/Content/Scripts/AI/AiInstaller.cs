47ea2270bb9d11e8d611d7d83a573aaee2f10f97
Nikita Puzerei <mykyta.puzieriei@tinybuild.com>
2026-01-04 00:46:14 +0200

    cameras

--- Assets/Content/Scripts/AI/AiInstaller.cs
+++ Assets/Content/Scripts/AI/AiInstaller.cs
@@ -1,15 +1,18 @@
+using System.Collections.Generic;
 using Content.Scripts.AI.GOAP;
 using Content.Scripts.AI.GOAP.Agent;
 using Content.Scripts.AI.GOAP.Goals;
 using Content.Scripts.AI.GOAP.Planning;
 using Reflex.Core;
 using Reflex.Extensions;
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Random = UnityEngine.Random;
 
 namespace Content.Scripts.Core {
   public class AiInstaller : MonoBehaviour, IInstaller {
+    public Transform agentsRoot;
     public GOAPAgent agentPrefab;
 
     public void InstallBindings(ContainerBuilder builder) {
@@ -17,7 +20,7 @@ namespace Content.Scripts.Core {
 
       builder.AddSingleton(new GoapPlanFactory());
       builder.AddSingleton(GoatFeatureBankModule.GetFromResources());
-      builder.AddSingleton(new AgentFactory(agentPrefab));
+      builder.AddSingleton(new AgentFactory(agentPrefab, agentsRoot));
     }
 
     // // // TEST // // //
@@ -28,7 +31,15 @@ namespace Content.Scripts.Core {
       var agentFactory = gameObject.scene.GetSceneContainer().Resolve<AgentFactory>();
       if (agentFactory != null) {
         var position = transform.position + Random.onUnitSphere * 5;
-        agentFactory.Spawn(position);
+        var instance = agentFactory.Spawn(position);
+
+        var camGroup = agentsRoot.GetComponent<CinemachineTargetGroup>();
+        camGroup.Targets ??= new List<CinemachineTargetGroup.Target>();
+        camGroup.Targets.Add(new CinemachineTargetGroup.Target() {
+          Object = instance.transform,
+          Radius = 5,
+          Weight = 1
+        });
       }
       else {
         Debug.LogError("np factory resolved");
--- Assets/Content/Scripts/AI/GOAP/Agent/AgentFactory.cs
+++ Assets/Content/Scripts/AI/GOAP/Agent/AgentFactory.cs
@@ -9,14 +9,17 @@ namespace Content.Scripts.AI.GOAP.Agent {
 
   public class AgentFactory : IAgentFactory {
     private readonly GOAPAgent _prefab;
+    private readonly Transform _root;
 
-    public AgentFactory(GOAPAgent prefab) {
+    public AgentFactory(GOAPAgent prefab, Transform agentsRoot) {
       _prefab = prefab;
+      _root = agentsRoot;
     }
 
     public GOAPAgent Spawn(Vector3 position) {
       var instance = Object.Instantiate(_prefab, position, Quaternion.identity);
       GameObjectInjector.InjectRecursive(instance.gameObject, instance.gameObject.scene.GetSceneContainer());
+      //instance.transform.SetParent(_root, true);
       instance.OnCreated();
       return instance;
     }
