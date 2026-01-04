6805971a966bf216a7ca635ae34eeb74b88177cb
Nikita Puzerei <mykyta.puzieriei@tinybuild.com>
2026-01-04 21:09:11 +0200

    ui

--- Assets/Content/Scripts/Ui/Layers/MainPanel/StatsPanel.cs
+++ Assets/Content/Scripts/Ui/Layers/MainPanel/StatsPanel.cs
@@ -5,20 +5,28 @@ namespace Content.Scripts.Ui.Layers.MainPanel {
   public class StatsPanel : BaseInfoPanel {
     [SerializeField] private StatPresenter _statPresenterPrefab;
     [SerializeField] private Transform _statsContainer;
+    [SerializeField] private BeliefPresenter _beliefPrefab;
+    [SerializeField] private Transform _container;
 
     public override void Repaint() {
       base.Repaint();
       _statsContainer.DestroyChildren();
+      _container.DestroyChildren();
       if (agent == null) {
         return;
       }
-
-      var body = agent.body;
-      var stats = body.GetStatsInfo();
-
+      
+      var stats = agent.body.GetStatsInfo();
       foreach (var stat in stats) {
         var presenter = Instantiate(_statPresenterPrefab, _statsContainer);
         presenter.Setup(stat);
+      }
+      
+      var beliefs = agent.agentBrain.beliefs;
+      foreach (var belief in beliefs.Values) {
+        if (!belief.lastEvaluation) continue;
+        var presenter = Instantiate(_beliefPrefab, _container);
+        presenter.Setup(belief);
       }
     }
   }
